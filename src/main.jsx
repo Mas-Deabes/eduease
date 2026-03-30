2d9b073c777fe57c5cc16bd40dc3a35344ad6e67
Sif El Din Deabes <62103437+Mas-Deabes@users.noreply.github.com>
2026-03-30 02:17:06 +0100

    Adding folder structure and global CSS design structure accross the site

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 import App from "./App"
+import "./styles/global.css" //importing the css here applies the theme to the entire app as this is the starting point
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+) d
