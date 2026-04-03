460299be579e3d0e6ac8144842f663dabbf86026
Sif El Din Deabes <62103437+Mas-Deabes@users.noreply.github.com>
2026-04-03 16:16:38 +0100

    Add AuthContext to manage global authentication state across the app

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./styles/global.css" //importing the css here applies the theme to the entire app as this is the starting point
+import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AuthProvider>
     <App />
+    </AuthProvider>
   </React.StrictMode>
 ) 
